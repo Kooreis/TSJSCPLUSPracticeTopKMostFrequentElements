1cb006a835911450ab1c18a72453744171719121
Kooreis <kooreis.official@gmail.com>
2023-04-25 06:24:06 -0500

    This update adds helper methods to the PriorityQueue class for managing the binary heap structure, including methods to get the parent, left child, and right child of a node, check if a node is present, and swap two nodes.

--- solution.cs
+++ solution.cs
@@ -1,36 +1,19 @@
-class PriorityQueue {
-    constructor(comparator = (a, b) => a > b) {
-        this._heap = [null];
-        this._comparator = comparator;
+_parent(idx) {
+        return Math.floor(idx / 2);
     }
 
-    size() {
-        return this._heap.length - 1;
+    _leftChild(idx) {
+        return idx * 2;
     }
 
-    isEmpty() {
-        return this.size() === 0;
+    _rightChild(idx) {
+        return idx * 2 + 1;
     }
 
-    peek() {
-        if (this.isEmpty()) return null;
-        return this._heap[1];
+    _isPresent(idx) {
+        return idx <= this.size();
     }
 
-    push(value) {
-        this._heap.push(value);
-        this._siftUp();
+    _swap(i, j) {
+        [this._heap[i], this._heap[j]] = [this._heap[j], this._heap[i]];
     }
-
-    pop() {
-        if (this.isEmpty()) return null;
-        const poppedValue = this._heap[1];
-        const bottom = this.size();
-        if (bottom > 1) {
-            this._swap(1, bottom);
-        }
-        this._heap.pop();
-        this._siftDown();
-        return poppedValue;
-    }
-}
